c9649108d57ecf28fa06e283f2bdab9e7698f11e
marlee99 <leemarianagonzales@gmail.com>
2023-05-10 10:38:58 -0300

    Agregue los contratados

--- src/ar/edu/unlam/pb2/test/parcialito/Contratado.java
+++ src/ar/edu/unlam/pb2/test/parcialito/Contratado.java
@@ -1,5 +1,23 @@
 package ar.edu.unlam.pb2.test.parcialito;
 
-public class Contratado {
+import java.time.LocalDate;
+
+public class Contratado extends Empleado {
+
+	private LocalDate fCaducidad;
+
+	public Contratado(String nombre, String apellido, Long CUIL, Long legajo, LocalDate fingreso, LocalDate fnac,
+			Departamento departamento, LocalDate fCaducidad, Credencial credencial) {
+		super(nombre, apellido, CUIL, legajo, fingreso, fnac, departamento, credencial);
+		this.fCaducidad = fCaducidad;
+	}
+
+	LocalDate getfCaducidad() {
+		return fCaducidad;
+	}
+
+	void setfCaducidad(LocalDate fCaducidad) {
+		this.fCaducidad = fCaducidad;
+	}
 
 }
